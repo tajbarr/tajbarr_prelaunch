26a4d92977cd65fc0bca2c4b8aa502fced25c477
Chris <melheffe@gmail.com>
2014-11-15 23:07:30 -0500

    Db and pw changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(:email => 'admin@prelaunchr.com', :password => 'changeme', :password_confirmation => 'changeme')
+AdminUser.create!(:email => 'info@tajbarr.com', :password => 'seo#3228', :password_confirmation => 'seo#3228')
